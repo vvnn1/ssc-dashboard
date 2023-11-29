5581ac5b596fa748471fa35b26942e3625e5ba0d
vvnn1_ <137418235@qq.com>
2023-11-29 14:56:09 +0800

    [feat] 完善元数据页面

--- src/page/MainPage/ContentLayout/MetaLayout/index.tsx
+++ src/page/MainPage/ContentLayout/MetaLayout/index.tsx
@@ -1,17 +1,20 @@
-import { Button, Input } from "antd";
+import { Button, Input, Tooltip } from "antd";
 import "./index.sass";
 import { PlusOutlined, ReloadOutlined, SearchOutlined } from "../../../../component/Icon";
 import MetaTree from "../DraftLayout/LeftTabBar/LeftTabPanel/MetaTabPanel/MetaTree";
 import ScrollPin from "../../../../component/ScrollPin";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import MetaDetailLayout from "./MetaDetailLayout";
 import { Route, Routes } from "react-router-dom";
 import CatalogDetailLayout from "./CatalogDetailLayout";
 import DatabaseDetailLayout from "./DatabaseDetailLayout";
 import TableDetailLayout from "./TableDetailLayout";
+import CreateCatalogModal from "./CreateCatalogModal";
+import { changeModalOpen } from "../../../../util";
 
 const MetaLayout = () => {
     const treeContainerRef = useRef<HTMLDivElement>(null);
+    const [createModalOpen, setCreateModalOpen] = useState<boolean>(false);
 
     return (
         <div className="meta-layout">
@@ -19,20 +22,25 @@ const MetaLayout = () => {
                 <div className="panel-bar header panel panel-ltr">
                     <span className="title">元数据</span>
                     <div className="actions">
-                        <Button
-                            className="ant-btn-icon-only"
-                            type="text"
-                            size="small"
-                        >
-                            <ReloadOutlined />
-                        </Button>
-                        <Button
-                            className="ant-btn-icon-only"
-                            type="text"
-                            size="small"
-                        >
-                            <PlusOutlined />
-                        </Button>
+                        <Tooltip title="刷新所有元数据信息">
+                            <Button
+                                className="ant-btn-icon-only"
+                                type="text"
+                                size="small"
+                            >
+                                <ReloadOutlined />
+                            </Button>
+                        </Tooltip>
+                        <Tooltip title="创建 Catalog">
+                            <Button
+                                className="ant-btn-icon-only"
+                                type="text"
+                                size="small"
+                                onClick={changeModalOpen(true, setCreateModalOpen)}
+                            >
+                                <PlusOutlined />
+                            </Button>
+                        </Tooltip>
                     </div>
                 </div>
                 <div className="panel-bar searchbar panel panel-ltr">
@@ -73,6 +81,10 @@ const MetaLayout = () => {
                     </Route>
                 </Routes>
             </div>
+            <CreateCatalogModal
+                open={createModalOpen}
+                onCancel={changeModalOpen(false, setCreateModalOpen)}
+            />
         </div>
     );
 };
