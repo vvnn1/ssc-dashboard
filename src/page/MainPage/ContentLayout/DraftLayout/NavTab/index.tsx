4f59378ef536061d56ad62fb03ce78b563bbb0f8
vvnn1_ <137418235@qq.com>
2023-12-30 22:50:52 +0800

    [feat] 添加草稿 Tab 标签上下文菜单

--- src/page/MainPage/ContentLayout/DraftLayout/NavTab/index.tsx
+++ src/page/MainPage/ContentLayout/DraftLayout/NavTab/index.tsx
@@ -1,9 +1,10 @@
-import { Tabs, TabsProps } from "antd";
+import { Dropdown, Tabs, TabsProps } from "antd";
 import { useEffect, useState } from "react";
 import "./index.sass";
 import { BatchDraftOutlined, StreamDraftOutlined } from "../../../../../component/Icon";
 import { useNavigate } from "react-router-dom";
 import CreateDraftModal from "../ToolBar/CreateDraftModal";
+import { Item, ItemProps, Menu, contextMenu, useContextMenu } from "react-contexify";
 
 type TargetKey = React.MouseEvent | React.KeyboardEvent | string;
 
@@ -19,9 +20,12 @@ interface NavTabProps {
     selectedItem?: TabItem;
 }
 
+const tabMenuId = "draft-tree-context-tab-menu";
+
 const NavTab: React.FC<NavTabProps> = ({ selectedItem }) => {
     const [items, setItems] = useState<TabItem[]>([]);
     const [modalOpen, setModalOpen] = useState<boolean>(false);
+    const { show: showTabMenu } = useContextMenu({ id: tabMenuId });
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -35,25 +39,34 @@ const NavTab: React.FC<NavTabProps> = ({ selectedItem }) => {
         }
     }, [selectedItem?.id]);
 
+    const onTabContextMenu = (item: TabItem) => {
+        return (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+            showTabMenu({ event: e, props: { targetId: item.id } });
+        };
+    };
+
     const transTabs = (items: TabItem[]): TabsProps["items"] => {
         return items.map(item => {
             return {
                 id: item.id,
                 key: item.id,
                 label: (
-                    <>
+                    <div
+                        className="custom-draft-tab"
+                        onContextMenu={onTabContextMenu(item)}
+                    >
                         <span className={`draft-type type ${item.hasDeploy ? "" : "offline"}`}>
                             {item.type === "s" ? <StreamDraftOutlined /> : null}
                             {item.type === "b" ? <BatchDraftOutlined /> : null}
                         </span>
                         <span className={`draft-name ${item.hasErr ? "has-error" : ""}`}>{item.name}</span>
-                    </>
+                    </div>
                 ),
             };
         });
     };
 
-    const onChange = (id: string) => {
+    const onSelectedChange = (id: string) => {
         navigate(`../../${id}/sql`);
     };
 
@@ -67,7 +80,7 @@ const NavTab: React.FC<NavTabProps> = ({ selectedItem }) => {
         setModalOpen(true);
     };
 
-    const remove = (targetId: TargetKey) => {
+    const removeItem = (targetId: TargetKey) => {
         let newActiveId = selectedItem?.id;
         let lastIndex = -1;
 
@@ -100,16 +113,49 @@ const NavTab: React.FC<NavTabProps> = ({ selectedItem }) => {
         if (action === "add") {
             add();
         } else {
-            remove(targetId);
+            removeItem(targetId);
         }
     };
 
+    const onCloseTabClick: ItemProps["onClick"] = ({ props: { targetId } }) => {
+        removeItem(targetId);
+    };
+
+    const onCloseRightClick: ItemProps["onClick"] = ({ props: { targetId } }) => {
+        const pickedItems: TabItem[] = [];
+        let onLeft = false;
+        for (let i = 0; i < items.length; i++) {
+            pickedItems.push(items[i]);
+
+            if (items[i].id === selectedItem?.id) {
+                onLeft = true;
+            }
+
+            if (items[i].id === targetId) {
+                setItems(pickedItems);
+                if (!onLeft) {
+                    navigate(`../../${items[i].id}/sql`, { replace: true });
+                }
+                return;
+            }
+        }
+    };
+
+    const onCloseOthersClick: ItemProps["onClick"] = ({ props: { targetId } }) => {
+        const item = items.find(item => item.id === targetId);
+        if (item) {
+            setItems([item]);
+            navigate(`../../${item.id}/sql`, { replace: true });
+        } else {
+            setItems([]);
+            navigate("../../.");
+        }
+    };
     return (
-        <>
+        <div className="editor-nav-tab">
             <Tabs
-                className="editor-nav-tab"
                 type="editable-card"
-                onChange={onChange}
+                onChange={onSelectedChange}
                 activeKey={selectedItem?.id}
                 onEdit={onEdit}
                 items={transTabs(items)}
@@ -119,7 +165,31 @@ const NavTab: React.FC<NavTabProps> = ({ selectedItem }) => {
                 open={modalOpen}
                 onCancel={changeModalOpen(false)}
             />
-        </>
+
+            <Menu
+                id={tabMenuId}
+                className="tab-context-menu"
+            >
+                <Item
+                    id="1"
+                    onClick={onCloseTabClick}
+                >
+                    关闭标签
+                </Item>
+                <Item
+                    id="2"
+                    onClick={onCloseOthersClick}
+                >
+                    关闭其他
+                </Item>
+                <Item
+                    id="3"
+                    onClick={onCloseRightClick}
+                >
+                    关闭右侧
+                </Item>
+            </Menu>
+        </div>
     );
 };
 
