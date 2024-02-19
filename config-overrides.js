f004322b9fd34a0adf080161ff2eba64f619fdbf
vvnn1_ <137418235@qq.com>
2024-02-19 15:59:24 +0800

    [build] 添加webpack打包分析

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,17 @@
 const PerspectivePlugin = require("@finos/perspective-webpack-plugin");
 const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const { override, addWebpackPlugin } = require("customize-cra");
-module.exports = override(addWebpackPlugin(new PerspectivePlugin()), addWebpackPlugin(new MonacoWebpackPlugin()));
+module.exports = override(
+    addWebpackPlugin(new PerspectivePlugin()),
+    addWebpackPlugin(
+        new MonacoWebpackPlugin({
+            languages: ["mysql"],
+        })
+    ),
+    addWebpackPlugin(
+        new BundleAnalyzerPlugin({
+            analyzerMode: "disabled",
+        })
+    )
+);
